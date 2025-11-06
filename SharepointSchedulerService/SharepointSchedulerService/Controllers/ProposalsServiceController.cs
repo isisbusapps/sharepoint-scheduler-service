7959444aa981478d9e474ad04c4f0c103fbb5a80
Dylan Val <dylan.val@stfc.ac.uk>
2025-11-06 13:13:23 +0000

    Setup SPSchedule Proposals Service endpoints

--- SharepointSchedulerService/SharepointSchedulerService/Controllers/ProposalsServiceController.cs
+++ SharepointSchedulerService/SharepointSchedulerService/Controllers/ProposalsServiceController.cs
@@ -13,5 +13,28 @@ namespace SharepointSchedulerService.Controllers
         {
             return Ok("Proposals service is up and running");
         }
+
+        [HttpGet("getClfRounds")]
+        public async Task<ActionResult<string>> GetClfRounds()
+        {
+            return Ok("List of CLF rounds returned here");
+            //Data here
+        }
+
+        [HttpGet("getIsisRounds")]
+        public async Task<ActionResult<string>> getIsisRounds()
+        {
+            return Ok("List of ISIS rounds returned here");
+            //Data here
+        }
+
+        [HttpGet("getExperimentReportsData")]
+        public async Task<ActionResult<string>> getExperimentReportsData(
+            [FromQuery] string facilityName,
+            [FromQuery] int fromYear)
+        {
+            return Ok($"Returning data for {facilityName} from year {fromYear}");
+        }
+
     }
 }
