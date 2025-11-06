309e06cc49d7b6bb6c67940208f31c17820012ba
Dylan Val <dylan.val@stfc.ac.uk>
2025-11-06 12:35:03 +0000

    Setting up controllers

---
+++ SharepointSchedulerService/SharepointSchedulerService/Controllers/VisitsServiceController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SharepointSchedulerService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VisitsServiceController : ControllerBase
+    {
+    }
+}
