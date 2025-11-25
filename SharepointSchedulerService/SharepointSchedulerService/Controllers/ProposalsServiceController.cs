d12c4361211fda5013a3ac0eeb72ccbb1e2149ae
Dylan Val <dylan.val@stfc.ac.uk>
2025-11-25 11:31:05 +0000

    Proposals endpoints methods create

--- SharepointSchedulerService/SharepointSchedulerService/Controllers/ProposalsServiceController.cs
+++ SharepointSchedulerService/SharepointSchedulerService/Controllers/ProposalsServiceController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SharepointSchedulerService.Models.DTOs;
 
 namespace SharepointSchedulerService.Controllers
 {
@@ -31,12 +33,33 @@ namespace SharepointSchedulerService.Controllers
 
         //GET /api/ProposalsService/getExperimentReportsData
         [HttpGet("getExperimentReportsData")]
-        public async Task<ActionResult<string>> getExperimentReportsData(
+        public async Task<ActionResult<List<ExperimentWithReportDTO>>> getExperimentReportsData(
             [FromQuery] string facilityName,
             [FromQuery] int fromYear)
         {
-            return Ok($"Returning data for {facilityName} from year {fromYear}");
-            //Data here
+            List<ExperimentWithReportDTO> experimentWithReportDTOs = new List<ExperimentWithReportDTO>();
+
+            try
+            {
+                SharepointDataAccess sharepointDataAccess = new SharepointDataAccess();
+                if (facilityName.Equals("ISIS"))
+                {
+                    experimentWithReportDTOs = AsyncContext.Run(() => sharepointDataAccess.GetISISExperimentalReportsListItems(fromYear));
+                }
+                else if (facilityName.Equals("CLF"))
+                {
+                    experimentWithReportDTOs.AddRange(AsyncContext.Run(() => sharepointDataAccess.GetHPLExperimentalReportsListItems(fromYear)));
+                    experimentWithReportDTOs.AddRange(AsyncContext.Run(() => sharepointDataAccess.GetLSFExperimentalReportsListItems(fromYear)));
+                    experimentWithReportDTOs.AddRange(AsyncContext.Run(() => sharepointDataAccess.GetArtemisExperimentalReportsListItems(fromYear)));
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Logger.WarnFormat("Could not look up experimental reports for facility name {0}: {1}", facilityName, ex);
+                return new List<ExperimentWithReportDTO>();
+            }
+
+            return experimentWithReportDTOs;
         }
     }
 }
---
+++ SharepointSchedulerService/SharepointSchedulerService/Helpers/SharepointDataAccess.cs
@@ -0,0 +1,84 @@
+﻿using log4net;
+using Microsoft.Extensions.Logging;
+using SharepointSchedulerService.Models.DTOs;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+namespace SharepointSchedulerService.Helpers
+{
+    public class SharepointDataAccess
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(SharepointDataAccess));
+
+        /*
+        public async Task<List<ExperimentWithReportDTO>> GetISISExperimentalReportsListItems(int fromYear)
+        {
+            //Not complete
+        }
+        */
+
+        private List<ExperimentWithReportDTO> JsonElementToExperimentWithReportDTO(List<JsonElement> jsonElements, string facility, int fromYear)
+        {
+            List<ExperimentWithReportDTO> experimentWithReportDTOs = new List<ExperimentWithReportDTO>();
+            foreach (JsonElement element in jsonElements)
+            {
+                string nameString = element.GetProperty("name").ToString();
+                string RbNumber = getRBNumberFromFileName(nameString);
+                string lastDateModifiedString = element.GetProperty("lastModifiedDateTime").ToString();
+                if (!string.IsNullOrEmpty(RbNumber) && isDateGreaterThanFromYear(fromYear, lastDateModifiedString))
+                {
+                    ExperimentWithReportDTO reportsDTO = new ExperimentWithReportDTO();
+                    reportsDTO.ExperimentNumber = RbNumber;
+                    reportsDTO.FacilityName = facility;
+                    reportsDTO.modifiedDate = DateTime.Parse(lastDateModifiedString);
+                    experimentWithReportDTOs.Add(reportsDTO);
+                }
+
+            }
+            return experimentWithReportDTOs;
+        }
+
+        private bool isDateGreaterThanFromYear(int year, string lastModifiedDate)
+        {
+            DateTime date = DateTime.Parse(lastModifiedDate);
+
+            if (date.Year >= year)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /*
+         * RB Numbers are stored in sharepoint in the column "Name" in the format: 
+         * #[0-9]+;(RB|App)<RB_NUMBER>.<FILE_EXTENSION>, we just want <RB_NUMBER>
+         */
+        private string getRBNumberFromFileName(string fileName)
+        {
+
+            Regex rbNumberRegex = new Regex("[aA-zZ]*(\\d+)");
+            Match rbNumberMatch;
+            string referenceNumber = string.Empty;
+            if (!string.IsNullOrWhiteSpace(fileName))
+            {
+                rbNumberMatch = rbNumberRegex.Match(fileName);
+
+                if (rbNumberMatch != null)
+                {
+                    // Group 0 is the whole thing, 1 is just the reference number part
+                    referenceNumber = rbNumberMatch.Groups[1].Value;
+                }
+                else
+                {
+                    Logger.WarnFormat("Ref number {0} not in decimal format as expected...leaving untouched", referenceNumber);
+                }
+            }
+            return referenceNumber;
+        }
+    }
+
+
+}
